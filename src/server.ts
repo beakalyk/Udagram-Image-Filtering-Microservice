66d1eb4971ac7556c34fc7fa22d8b21f57eaade9
beakalyk <50494009+beakalyk@users.noreply.github.com>
2022-09-11 00:28:01 +0300

    Update server.ts
    up

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,6 @@
-import express, {Request, Response} from 'express';
+import express from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
-const url = require('url');
 
 (async () => {
 
@@ -10,10 +9,7 @@ const url = require('url');
 
   // Set the network port
   const port = process.env.PORT || 8082;
-
-  // Supported image exttensions from https://www.npmjs.com/package/jimp
-  const jimpSuportedList: string[] = ['jpg', 'png', 'bmp', 'tiff', 'gif'];
-
+  
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
@@ -34,30 +30,7 @@ const url = require('url');
   /**************************************************************************** */
 
   //! END @TODO1
-  app.get("/filteredimage/", async (req: Request, res: Response) => {
-     let { image_url } = req.query;
-    
-     if(! image_url){
-       return res.status(400).send("Missing image URL.");
-     }
-
-     let parsedURL = url.parse(image_url, true);
-     
-     
-     if(!parsedURL.protocol || !parsedURL.slashes || !parsedURL.hostname || !parsedURL.pathname){
-       return res.status(400).send("Malformed URL.");
-
-     } 
-     if(jimpSuportedList.indexOf(parsedURL.pathname.split(".")[1]) === -1){
-       return res.status(415).send("Image extension is not supported");
-     }
-
-    
-     let filteredImageURI: string  = await filterImageFromURL(image_url);
-     res.status(200).sendFile(filteredImageURI);
-     res.on('finish', () => deleteLocalFiles([filteredImageURI]));
-});
-
+  
   // Root Endpoint
   // Displays a simple message to the user
   app.get( "/", async ( req, res ) => {
