1f03b3b6bebb5f5ebcb1653169ebc4b683c6371b
beakalyk <akaabe@hotmail.com>
2022-09-12 22:03:03 +0300

    unsupported extension issue fix evidence2

--- src/server.ts
+++ src/server.ts
@@ -11,8 +11,7 @@ const url = require('url');
   // Set the network port
   const port = process.env.PORT || 8082;
 
-  // Supported image exttensions from https://www.npmjs.com/package/jimp
-  const jimpSuportedList: string[] = ['jpg', 'png', 'bmp', 'tiff', 'gif'];
+  
 
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
